d76412264382b01297bdef6cc9db66e872c12111
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-10-14 02:06:47 +0100

    added basic REST verb functions to route

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -1,6 +1,18 @@
 'use strict'
 
-let server;
+const hh = require('../')
+const Joi = require('joi')
+const Promise = require('bluebird')
+
+let server, buildServer, destroyServer;
+
+const schema = {
+	type: 'brands',
+	attributes: {
+		code: Joi.string().min(2).max(10),
+		description: Joi.string()
+	}
+};
 
 describe('Plugin', function() {
 	beforeEach(function(done) {
@@ -10,19 +22,40 @@ describe('Plugin', function() {
 	afterEach(function(done) {
 		destroyServer(done);
 	})
+	
 	it('Attaches the plugin to Hapi server configuration', function() {
 		expect(server.plugins.harvester.version).to.equal('0.1.0')
-	});
+	})
 	
-	it('should fail with a descriptive error', function() {
+	it('should have the injectThen method available', function() {
   		return server.injectThen({method: 'GET', url: '/chuck'})
-		.then(function(res) {
-			expect(res.result).to.deep.equal({ statusCode: 404, error: 'Not Found' });
-		});
-	});
+		.then((res) => {
+			expect(res.result).to.deep.equal({ statusCode: 404, error: 'Not Found' })
+		})
+	})
+	
+	it('all the REST verbs available', function() {
+
+		const hh = server.plugins.harvester;
+		
+		let promises = [];
+
+		['get', 'put', 'post', 'patch', 'delete'].forEach(function(verb) {
+			server.route(hh.routes[verb](schema))
+			
+			let promise = server.injectThen({method: verb.toUpperCase(), url: '/brands'}).then((res) => {
+				expect(res.result).to.deep.equal({ foo: 'bar' })
+			})
+			
+			promises.push(promise)
+		})
+		
+		return server.injectThen({method: 'GET', url: '/chuck'})
+		return Promise.all(promises)
+	})
 })
 
-var buildServer = function(done) {
+buildServer = function(done) {
 	const Hapi = require('hapi')
 	server = new Hapi.Server()
     server.connection()
@@ -32,6 +65,6 @@ var buildServer = function(done) {
 	], done)
 }
 
-var destroyServer = function(done) {
+destroyServer = function(done) {
 	server.stop(done)
 }
