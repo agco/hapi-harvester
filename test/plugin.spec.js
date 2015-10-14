f00cf3b71cab369dd4b646e1ac157e98e4c6b1ea
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-10-14 01:12:03 +0100

    plugin up and our first test is running

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -3,23 +3,23 @@
 let server;
 
 describe('Plugin', function() {
-	beforeEach(function() {
-		buildServer();
+	beforeEach(function(done) {
+		buildServer(done);
 	})
 	
 	afterEach(function(done) {
 		destroyServer(done);
 	})
 	it('Attaches the plugin to Hapi server configuration', function() {
-		console.log(server.plugins)
+		expect(server.plugins.harvester.version).to.equal('0.1.0')
 	});
 })
 
-var buildServer = function() {
+var buildServer = function(done) {
 	const Hapi = require('hapi')
 	server = new Hapi.Server()
     server.connection()
-    return server
+	server.register({register: require('../lib/plugin')}, done)
 }
 
 var destroyServer = function(done) {
