ecab7c0d66acca4ff9f970be8f29c6c90dda06e9
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-10-14 01:25:14 +0100

    got inject-then working with mocha tests

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -13,13 +13,23 @@ describe('Plugin', function() {
 	it('Attaches the plugin to Hapi server configuration', function() {
 		expect(server.plugins.harvester.version).to.equal('0.1.0')
 	});
+	
+	it('should fail with a descriptive error', function() {
+  		return server.injectThen({method: 'GET', url: '/chuck'})
+		.then(function(res) {
+			expect(res.result).to.deep.equal({ statusCode: 404, error: 'Not Found' });
+		});
+	});
 })
 
 var buildServer = function(done) {
 	const Hapi = require('hapi')
 	server = new Hapi.Server()
     server.connection()
-	server.register({register: require('../lib/plugin')}, done)
+	server.register([
+		{register: require('../lib/plugin')},
+		{register: require('inject-then')}
+	], done)
 }
 
 var destroyServer = function(done) {
