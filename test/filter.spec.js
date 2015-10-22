aa09deb60ef44ad27c80580a709e737737fade12
Mehdi Avdi <mehdi.avdi@gmail.com>
2015-10-22 02:46:01 +0100

    added filter validation

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -11,6 +11,8 @@ const schema = {
     type: 'brands',
     attributes: {
         code: Joi.string().min(2).max(10),
+        year: Joi.number(),
+        series: Joi.number(),
         description: Joi.string()
     }
 };
@@ -18,13 +20,15 @@ const schema = {
 const data = {
     attributes: {
         code: 'MF',
+        year: 2007,
+        series: 5,
         description: 'Massey Furgeson'
     }
 };
 
 
 //TODO just done the validation, actual includes is remaining
-describe('Paging', function() {
+describe('Filtering', function() {
     
     beforeEach(function(done) {
         buildServer(() => {
@@ -45,8 +49,8 @@ describe('Paging', function() {
         destroyServer(done)
     })
     
-    it('Will be able to GET all from /brands with a paging param', function() {
-        return server.injectThen({method: 'get', url: '/brands?include=code&page[limit]=100'})
+    it('Will be able to GET all from /brands with a filtering param', function() {
+        return server.injectThen({method: 'get', url: '/brands?include=code&filter[year]=gt=2006'})
         .then((res) => {
             res.result.data.forEach((data) => {
                 expect(data.id).to.match(/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/)
@@ -55,8 +59,8 @@ describe('Paging', function() {
         })
     })
     
-    it('Will be able to GET all from /brands with multiple paging params', function() {
-        return server.injectThen({method: 'get', url: '/brands?page[offset]=1000&page[limit]=100'})
+    it('Will be able to GET all from /brands with multiple filtering params', function() {
+        return server.injectThen({method: 'get', url: '/brands?filter[year]=lt=2010&filter[series]=gt=2'})
         .then((res) => {
             res.result.data.forEach((data) => {
                 expect(data.id).to.match(/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/)
@@ -65,9 +69,9 @@ describe('Paging', function() {
         })
     })
     
-    it('Won\'t be able to GET all from /brands with multiple paging params where one is not available in attributes', function() {
+    it('Won\'t be able to GET all from /brands with multiple filtering params where one is not available in attributes', function() {
         
-        return server.injectThen({method: 'get', url: '/brands?page[foo]=bar&page[limit]=100'})
+        return server.injectThen({method: 'get', url: '/brands?filter[foo]=ge=2007&filter[year]=gt=2000'})
         .then((res) => {
             expect(res.statusCode).to.equal(400)  
         })
