d2c21d611b697c5043b944f0a78ee29bb8f7f908
sajdakk <kristof.sajdak@gmail.com>
2016-01-20 11:52:31 +0100

    added some tests for adapter + refactoring removed a redundant register of schemas on the plugin namespace

--- test/adapter.spec.js
+++ test/adapter.spec.js
@@ -5,29 +5,41 @@ const Hapi = require('hapi')
 const utils = require('./utils');
 const config = require('./config');
 
-describe('Adapter Validation', function () {
+describe.only('Adapter Validation', function () {
 
     const harvester = require('../')
     const mongodbAdapter = harvester.getAdapter('mongodb')
     const mongodbSSEAdapter = harvester.getAdapter('mongodb/sse')
 
     it('Will succeed if passed a valid adapter ', function () {
-
-        expect(buildServerSetupWithAdapters(
-                mongodbAdapter(config.mongodbUrl),
-                mongodbSSEAdapter(config.mongodbOplogUrl)
-            )).to.not.throw(Error)
+        return buildServerSetupWithAdapters(mongodbAdapter(config.mongodbUrl), mongodbSSEAdapter(config.mongodbOplogUrl))
     })
 
-    it('Will fail if the given adapter is missing a required function', function () {
+    it('Will fail if the given adapter is missing a required function', function (done) {
 
-        let adapter = mongodbAdapter(config.mongodbUrl)
-        adapter = _.omit(adapter, 'delete');
-        expect(buildServerSetupWithAdapters(
-            adapter,
-            mongodbSSEAdapter(config.mongodbOplogUrl)
-        )).to.throw('Adapter validation failed. Adapter missing delete')
+        const adapter = mongodbAdapter(config.mongodbUrl)
+        const adapterWithoutDelete = _.omit(adapter, 'delete');
+        buildServerSetupWithAdapters(adapterWithoutDelete, mongodbSSEAdapter(config.mongodbOplogUrl))
+            .catch(e => {
+                expect(e.message).to.equal('Adapter validation failed. Adapter missing delete')
+                done()
+            })
+            .catch((e) => done(e))
     })
+
+    it('will initialise the adapter with provided options', function () {
+        return initialiseAdapterWithOptions(mongodbAdapter(config.mongodbUrl, {server: {maxPoolSize: 10}}))
+    })
+
+    it('will initialise the adapterSSE with provided options', function () {
+        return initialiseAdapterWithOptions(mongodbSSEAdapter(config.mongodbUrl, {server: {maxPoolSize: 10}}))
+    })
+
+    function initialiseAdapterWithOptions(adapterInstance) {
+        return adapterInstance.connect().then(()=> {
+            adapterInstance.disconnect()
+        })
+    }
 
     it('Will won\'t accept a string adapter if it doesn\'t exist ', function () {
         function constructAdapter() {
@@ -39,20 +51,24 @@ describe('Adapter Validation', function () {
 })
 
 function buildServerSetupWithAdapters(adapter, adapterSSE) {
-    return function () {
+    return new Promise((resolve, reject)=> {
         var server = new Hapi.Server()
         server.connection()
 
         server.register([
-            {register: require('../'), options: {
+            {
+                register: require('../'), options: {
                 adapter: adapter,
                 adapterSSE: adapterSSE
-            }},
+            }
+            },
             {register: require('inject-then')}
         ], () => {
-            server.start(()=> {
+            server.start((err)=> {
+                if (err) reject(err)
+                else resolve(server)
             })
         })
-    }
+    })
 }
 
